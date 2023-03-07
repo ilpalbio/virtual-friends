2b68aa98b8efa615edaf8b6e75f266c49d01b42a
ilpalbio <ilpalbio@gmail.com>
2023-03-07 16:24:48 +0100

    add migrations for db

---
+++ database/migrations/avatar/2023_03_07_133245_create_avatar_info.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('avatar_info', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_avatar')->unique();
+            $table->string('name');
+            $table->string('surname');
+            $table->tinyInteger('age');
+            $table->timestamp('last_call')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('avatar_info');
+    }
+};
